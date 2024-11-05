ed5622a4df0d5aeb5bf5bd4c9a75cb295a66d1da
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2024-11-05 12:42:10 +0530

    Time: 2 ms (37.45%), Space: 9.5 MB (18.24%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+#include <cctype> // for isalpha(), tolower()
+#include <algorithm> // for reverse()
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        string snew;
+        for (char c : s) {
+            if (isalnum(c)) { 
+                snew.push_back(tolower(c)); 
+            }
+        }
+
+        string r = snew;
+        reverse(r.begin(), r.end());
+        return snew == r;
+    }
+};
