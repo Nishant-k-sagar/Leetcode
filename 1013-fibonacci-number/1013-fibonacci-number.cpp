8464e005c808ebc53ef931e64c10e1a626a436c6
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2024-11-27 23:08:04 +0530

    Time: 0 ms (100%), Space: 7.6 MB (11.28%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -3,6 +3,15 @@ public:
     int fib(int n) {
         if(n==0)return 0;
         if(n==1)return 1;
-        return fib(n-1)+fib(n-2);
+        int prev1=0;
+        int prev2=1;
+        int curr=0;
+        for (int i = 1; i < n; i++) {
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+
+        return curr;
     }
 };
