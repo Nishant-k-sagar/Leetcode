8370a937905ccb4d72885af511245c44f259c3bc
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2024-12-10 18:42:25 +0530

    Time: 19 ms (44.3%), Space: 12.2 MB (69.42%) - LeetHub

--- 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/3267-find-longest-special-substring-that-occurs-thrice-i.cpp
@@ -1,38 +1,28 @@
 class Solution {
-    int &getMin(int &a, int &b, int &c){
-        if(a<=b && a<=c)return a;
-        if(b<=a && b<=c)return b;
-        return c;
-    }
-
 public:
     int maximumLength(string s) {
-        int n = s.size();
-        int maxLen = -1;
+        map<string, int> mp;
 
-        vector<vector<int>>top3freq(26, vector<int>(3, -1));
-
-        char last_seen = '%';
-
-        int win_len = 0;
-
-        for( char &curr : s){
-            int idx = curr - 'a';
-            win_len = curr == last_seen?win_len+1:1;
-
-            last_seen = curr;
-
-            int &minimum = getMin(top3freq[idx][0], top3freq[idx][1], top3freq[idx][2]);
-            if(win_len > minimum){
-                minimum = win_len;
+        int n = s.length();
+        for(int i=0; i<n; i++){
+            string scurr;
+            for(int j = i; j<n; j++){
+                if(scurr.empty() or scurr.back() == s[j]){
+                    scurr.push_back(s[j]);
+                    mp[scurr]++;
+                }
+                else break;
             }
-
-            
-
-        
-            for(int i=0; i<26; i++)
-                maxLen = max(maxLen, min({top3freq[i][0], top3freq[i][1], top3freq[i][2]}));
         }
-                return maxLen;
+
+        int ans = 0;
+
+        for(auto i : mp){
+            string str = i.first;
+            if(i.second >= 3 && str.length()>ans)ans = str.length();
+        }
+
+        if(ans == 0)return -1;
+        return ans;
     }
 };
