2c3ed19e9f84ef555b397565aa3f0b0b71e06eb9
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2024-12-11 22:45:52 +0530

    Time: 58 ms (51.66%), Space: 104.9 MB (84.24%) - LeetHub

--- 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/2891-maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -1,36 +1,21 @@
 class Solution {
-
-    int binarySearch(vector<int> &nums, int target, int n){
-        int left = 0;
-        int right = n-1;
-
-        while(left<=right){
-            int mid = (left+right)/2;
-
-            if(nums[mid]<=target){
-                left = mid+1;
-            }
-            else{
-                right = mid-1;
-            }
-        }
-        return right;
-    }
 public:
     int maximumBeauty(vector<int>& nums, int k) {
         int n = nums.size();
 
         sort(nums.begin(), nums.end());
 
+        int i = 0;
+        int j = 0;
+
         int maxBeauty = 0;
 
-        for(int i=0; i<n; i++){
-            int x = nums[i];
-            int y = x + 2*k;
-
-            int j = binarySearch(nums, y, n);
-
-            maxBeauty = max(maxBeauty, j-i+1);
+        while(i<n){
+            while(j<n and nums[j]<=nums[i]+2*k){
+                j++;
+            }
+            maxBeauty = max(maxBeauty, j-i);
+            i++;
         }
         return maxBeauty;
     }
