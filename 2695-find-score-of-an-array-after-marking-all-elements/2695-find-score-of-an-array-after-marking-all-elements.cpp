e511c2aa2f3fb14ec657ee149b0aaecdcf67e2be
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2024-12-13 22:37:33 +0530

    Time: 55 ms (93.86%), Space: 98.8 MB (90.85%) - LeetHub

--- 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
+++ 2695-find-score-of-an-array-after-marking-all-elements/2695-find-score-of-an-array-after-marking-all-elements.cpp
@@ -2,37 +2,30 @@ class Solution {
 public:
     long long findScore(vector<int>& nums) {
         int n = nums.size();
-        long long ans=0;
-
-        vector<bool> marked(n, false);
-
-        auto cmp = [](pair<int, int> &pair1, pair<int, int> &pair2){
-            if(pair1.first != pair2.first)return pair1.first > pair2.first;
-            return pair1.second> pair2.second;
-        };
-
-        priority_queue<pair<int, int>, vector<pair<int, int>>, decltype(cmp)>heap(cmp);
+        
+         long ans = 0;
+        vector<pair<int, int>>sorted(n);
 
         for(int i=0; i<n; i++){
-            heap.push(make_pair(nums[i], i));
+            sorted[i] = make_pair(nums[i], i);
         }
 
-        while(!heap.empty()){
-            pair<int, int> ele = heap.top();
+        sort(sorted.begin(), sorted.end());
 
-            heap.pop();
+        vector<bool>marked(n, false);
 
-            int num = ele.first;
-            int idx = ele.second;
+        for(int i=0; i<n; i++){
+            int num = sorted[i].first;
+            int idx = sorted[i].second;
 
             if(!marked[idx]){
-                ans+=num;
+                ans = ans+num;
                 marked[idx] = true;
 
-                if(idx-1>=0){
+                if(idx>=1){
                     marked[idx-1] = true;
                 }
-                if(idx+1<n){
+                if(idx<=n-1){
                     marked[idx+1] = true;
                 }
             }
