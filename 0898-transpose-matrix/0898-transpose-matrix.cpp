ae9f2efaad3fee5a0c30be9317c04cffeceb18e0
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2024-12-16 19:48:12 +0530

    Time: 0 ms (100%), Space: 15.1 MB (18.75%) - LeetHub

--- 0898-transpose-matrix/0898-transpose-matrix.cpp
+++ 0898-transpose-matrix/0898-transpose-matrix.cpp
@@ -6,9 +6,9 @@ public:
 
         vector<vector<int>> res(m, vector<int>(n, 0));
 
-        for(int i =0; i<n; i++){
-            for(int j = 0; j<m; j++){
-                res[j][i] = matrix[i][j];
+        for(int i =0; i<m; i++){
+            for(int j = 0; j<n; j++){
+                res[i][j] = matrix[j][i];
             }
         }
         return res;
