7b4a15520b48a3cf476a59d1c3e602b4416b36af
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-01-06 23:38:24 +0530

    Time: 0 ms (100%), Space: 7.7 MB (90.26%) - LeetHub

--- 0052-n-queens-ii/0052-n-queens-ii.cpp
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
+
     int queens[10];
 
-    bool possible(int row, int col) {
-        for (int i = 0; i < row; i++) {
-            int prow = i;
+    bool possible(int row, int col){
+        for(int i=0; i<row; i++){
             int pcol = queens[i];
 
-            if (pcol == col || abs(prow - row) == abs(pcol - col)) {
-                return false;
+            if(pcol==col || abs(i-row) == abs(pcol- col)){
+                return 0;
             }
         }
-        return true;
+        return 1;
     }
 
-    int rec(int row, int n) {
-        if (row == n) {
-            return 1;
-        }
+    int rec(int row, int n){
+        if(row==n)return 1;
 
         int ans = 0;
 
-        for (int col = 0; col < n; col++) {
-            if (possible(row, col)) {
-                queens[row] = col;  
-                ans += rec(row + 1, n);
+        for(int col = 0; col<n; col++){
+            if(possible(row, col)){
+                queens[row] = col;
+                ans+=rec(row+1, n);
                 queens[row] = -1;
             }
         }
@@ -34,5 +32,6 @@ public:
     int totalNQueens(int n) {
         memset(queens, -1, sizeof(queens));
         return rec(0, n);
+        // return val;
     }
 };
