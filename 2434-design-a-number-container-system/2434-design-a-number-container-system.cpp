993b2dcc2e9ef76b050beaf4a709c2a76a31cdea
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-09 02:57:17 +0530

    Time: 133 ms (29.55%), Space: 196 MB (45.07%) - LeetHub

--- 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
+++ 2434-design-a-number-container-system/2434-design-a-number-container-system.cpp
@@ -1,30 +1,40 @@
 class NumberContainers {
 public:
-    unordered_map<int, int> idxToNum; 
-    unordered_map<int, priority_queue<int, vector<int>, greater<int>>> numToIdx;
+    
+    unordered_map<int, int> idxToNum;
+    unordered_map<int, set<int >> numToIdx;
 
     NumberContainers() {
+        //do nothing
+        idxToNum.clear();
+        numToIdx.clear();
     }
     
     void change(int i, int num) {
-        if (idxToNum.count(i)) {
+        if(idxToNum.count(i)){
             int prevNum = idxToNum[i];
-            numToIdx[prevNum].push(i);
+            numToIdx[prevNum].erase(i);
+
+            if(numToIdx[prevNum].empty()){
+                numToIdx.erase(prevNum);
+            }
         }
-        
+
         idxToNum[i] = num;
-        numToIdx[num].push(i);
+        numToIdx[num].insert(i);
     }
     
     int find(int num) {
-        while (!numToIdx[num].empty()) {
-            int idx = numToIdx[num].top();
-            if (idxToNum[idx] == num) {
-                return idx;
-            } else {
-                numToIdx[num].pop();
-            }
+        if(numToIdx.count(num)){
+             return * numToIdx[num].begin();
         }
-        return -1;
+    return -1;
     }
 };
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
