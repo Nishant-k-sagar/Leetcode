23da2ba7deb52cb11f2b6f809884cb1d3ea2cca7
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-10 01:57:54 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2541-sum-of-number-and-its-reverse/2541-sum-of-number-and-its-reverse.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool sumOfNumberAndReverse(int num) {
+        // int n = num.size();
+
+        for(int i=0; i<num; i++){
+            int temp = i;
+            string s = to_string(temp);
+            reverse(s.begin(), s.end());
+
+            if(temp + stoi(s)==num)return true;
+        }
+        return false;
+    }
+};
