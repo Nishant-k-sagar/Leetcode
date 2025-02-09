3e04836edbfbb2ecf6301505e6ccde1d6686a323
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-10 01:28:33 +0530

    Time: 62 ms (80.18%), Space: 92.6 MB (19.82%) - LeetHub

--- 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
+++ 2448-count-number-of-bad-pairs/2448-count-number-of-bad-pairs.cpp
@@ -5,20 +5,25 @@ public:
         int n = nums.size();
         ll res = 0;
 
-        for (ll i = 0; i < n; i++) {
-            nums[i] = nums[i] - i;
+        for(ll i = 0; i<n; i++){
+            nums[i] = nums[i]-i;
         }
 
         unordered_map<ll, ll> mp;
 
-        for (ll j = 0; j < n; j++) {
-            ll cntNumsj = mp[nums[j]]++;  
+        // mp[nums[0]] = 1;
+
+        for(ll j=0; j<n; j++){
+            // mp[nums[j]]++;
+            ll cntNumsj = mp[nums[j]]++;
             ll totNumsBeforej = j;
 
             ll badPairs = totNumsBeforej - cntNumsj;
 
-            res += badPairs;
+            res+= badPairs;
+
         }
         return res;
+        
     }
 };
