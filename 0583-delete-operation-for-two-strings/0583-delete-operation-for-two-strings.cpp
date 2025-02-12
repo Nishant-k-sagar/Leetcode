94df6e7118b1426bc6fd80fac6ed4ca7acc13528
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-12 18:45:27 +0530

    Time: 7 ms (86.87%), Space: 11.5 MB (90.65%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
 
-    int helper(string &s, string &t, vector<vector<int>> &dp){
+    int helper(string &s, string &t, vector<int> &cur, vector<int> &prev){
         int n = s.length();
         int m = t.length();
 
-        for(int i=0; i<=n; i++){
-            dp[i][0] = 0;
-        }
-
-        for(int j = 0; j<=m; j++){
-            dp[0][j] = 0;
-        }
-
         for(int i=1; i<=n; i++){
             for(int j=1; j<=m; j++){
-                if(s[i-1] == t[j-1])dp[i][j] = 1 + dp[i-1][j-1];
-                else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
+                if(s[i-1] == t[j-1])cur[j] = 1 + prev[j-1];
+                else cur[j] = max(prev[j], cur[j-1]);
             }
+            prev = cur;
         }
-        return dp[n][m];
+        return cur[m];
     }
 
     int minDistance(string w1, string w2) {
         int n = w1.length();
         int m = w2.length();
 
-        vector<vector<int>> dp(n+1, vector<int> (m+1, -1));
+        vector<int> cur(m+1, 0), prev(m+1, 0);
 
-        return n + m - 2 * helper(w1, w2, dp);
+        return n + m - 2 * helper(w1, w2, cur, prev);
     }
 };
