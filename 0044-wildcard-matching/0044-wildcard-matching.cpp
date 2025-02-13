33faa0cb1dc4601715b13c4e76ccfa2aa06326ef
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-13 18:04:25 +0530

    Time: 18 ms (85.32%), Space: 10.2 MB (85.18%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -3,37 +3,37 @@ public:
     bool isMatch(string s, string p) {
         int n = p.length();
         int m = s.length();
-
-        vector<vector<bool>> dp(n+1, vector<bool>(m+1, false));
-        dp[0][0] = true;
-
-        for(int j = 1; j<=m; j++){
-            dp[0][j] = false;
+        
+        vector<bool> prev(m + 1, false), cur(m + 1, false);
+        
+        prev[0] = true;
+        
+        for (int i = 1; i <= n && p[i-1] == '*'; i++) {
+            prev[0] = true;
         }
-
-        for(int i=1; i<=n; i++){
-            bool f = true;
-            for(int idxi=1; idxi<=i; idxi++){
-                if(p[idxi-1] != '*'){
-                    f = false;
-                    break;
+        
+        for (int i = 1; i <= n; i++) {
+            if (p[i-1] == '*') {
+                cur[0] = prev[0];
+            } else {
+                cur[0] = false;
+            }
+            
+            for (int j = 1; j <= m; j++) {
+                if (p[i-1] == '*') {
+                    cur[j] = prev[j] || cur[j-1];
+                }
+                else if (p[i-1] == '?' || p[i-1] == s[j-1]) {
+                    cur[j] = prev[j-1];
+                }
+                else {
+                    cur[j] = false;
                 }
             }
-            dp[i][0] = f;
+            
+            prev = cur;
         }
-
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=m; j++){
-                if (p[i-1] == s[j-1] || p[i-1] == '?') {
-                   dp[i][j] = dp[i - 1][j - 1];
-            }
-
-                if (p[i-1] == '*') {
-                    dp[i][j] = dp[i - 1][j] || dp[i][j - 1];
-        }
-            }
-        }
-
-        return dp[n][m];
+        
+        return prev[m];
     }
 };
