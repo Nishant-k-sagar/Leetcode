d4e4e6247fd325ff518b95111dcc13717453408d
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-13 17:29:37 +0530

    Time: 24 ms (78.38%), Space: 31.3 MB (38.03%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     int check(int i, int j, string &p, string &s, vector<vector<int>> &dp) {
-        if (i < 0 && j < 0) return true;
-        if (i < 0 && j >= 0) return false;
+        if (i == 0 && j == 0) return true;
+        if (i == 0 && j > 0) return false;
 
-        if (j < 0) {
-            for (int idxi = 0; idxi <= i; idxi++) {
-                if (p[idxi] != '*') return false;
+        if (j == 0 && i>0) {
+            for (int idxi = 1; idxi <= i; idxi++) {
+                if (p[idxi-1] != '*') return false;
             }
             return true;
         }
 
         if(dp[i][j] != -1) return dp[i][j];
 
-        if (p[i] == s[j] || p[i] == '?') {
+        if (p[i-1] == s[j-1] || p[i-1] == '?') {
             return dp[i][j] = check(i - 1, j - 1, p, s, dp);
         }
 
-        if (p[i] == '*') {
+        if (p[i-1] == '*') {
             return dp[i][j] = check(i - 1, j, p, s, dp) || check(i, j - 1, p, s, dp);
         }
 
@@ -28,7 +28,7 @@ public:
         int n = p.length();
         int m = s.length();
 
-        vector<vector<int>> dp(n, vector<int>(m, -1));
-        return check(n - 1, m - 1, p, s, dp);
+        vector<vector<int>> dp(n+1, vector<int>(m+1, -1));
+        return check(n, m, p, s, dp);
     }
 };
