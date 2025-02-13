0cb256df9d0c7e82f76e3c7d432e3dcfc2cbc818
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-13 14:07:49 +0530

    Time: 7 ms (59.62%), Space: 10.2 MB (99.22%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -4,21 +4,22 @@ public:
         int n = w1.length();
         int m = w2.length();
 
-        vector<vector<int>> dp(n+1, vector<int> (m+1 , 0));
+        vector<int> prev(m+1, 0), cur(m+1, 0);
 
-        for(int i = 0; i<=n; i++)dp[i][0] = i;
-        for(int j = 0; j<=m; j++)dp[0][j] = j;
+        for(int j=0; j<=m; j++) prev[j] = j;
 
         for(int i = 1; i<=n; i++){
+            cur[0] = i;
             for(int j = 1; j<=m; j++){
                 if(w1[i-1] == w2[j-1]){
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
                 }
                 else {
-                    dp[i][j] = min(1 + dp[i-1][j-1], min(1 + dp[i-1][j], 1 + dp[i][j-1]));
+                    cur[j] = min(1 + prev[j-1], min(1 + prev[j], 1 + cur[j-1]));
                 }
             }  
+            prev = cur;
         }
-        return dp[n][m];
+        return prev[m];
     }
 };
