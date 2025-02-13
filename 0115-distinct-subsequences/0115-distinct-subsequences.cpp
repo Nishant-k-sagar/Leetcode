1f97fac98f3d0c9b73f49aec48f04175f66e7ca9
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-13 11:45:01 +0530

    Time: 47 ms (14.09%), Space: 9.1 MB (83.78%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -6,18 +6,16 @@ public:
         ld n = s.length();
         ld m = t.length();
 
-        vector<ld> cur(m+1, 0), prev(m+1, 0);
-        prev[0] = cur[0] = 1;
+        vector<ld> cur(m+1, 0);
+        cur[0] = 1;
 
         for(ld i=1; i<=n; i++){
-            for(ld j=1; j<=m; j++){
+            for(ld j=m; j>=1; j--){
                 if(s[i-1] == t[j-1]){
-                    cur[j] = prev[j-1] + prev[j];
+                    cur[j] = cur[j-1] + cur[j];
                 }
-                else cur[j] = prev[j];
             }
-            prev = cur;
         }
-        return int(prev[m]);
+        return int(cur[m]);
     }
 };
