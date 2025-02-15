6571c32cbda50edd98f385dadeb23b9b7ff89e0b
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-15 12:27:45 +0530

    Time: 0 ms (100%), Space: 15.9 MB (66.17%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -6,12 +6,8 @@ public:
 
         for(int i = n-1; i>=0; i--){
             for(int buy = 0; buy<=1; buy++){
-                if(buy == 1){
-                    dp[i][buy] = max(-p[i] + dp[i+1][0], dp[i+1][1]);
-                }
-                else {
-                    dp[i][buy] = max(p[i] + dp[i+2][1], dp[i+1][0]);
-                }
+                dp[i][1] = max(-p[i] + dp[i+1][0], dp[i+1][1]);
+                dp[i][0] = max(p[i] + dp[i+2][1], dp[i+1][0]);
             }
         }
         return dp[0][1];
