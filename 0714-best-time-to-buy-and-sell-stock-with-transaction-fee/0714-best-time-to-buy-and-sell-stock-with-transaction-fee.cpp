7546f985ec8b1fd74e548db23797f1dee0704e3e
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-15 13:56:37 +0530

    Time: 0 ms (100%), Space: 58.8 MB (87.43%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -1,20 +1,19 @@
 class Solution {
 public:
-    int cnt(int idx, int buy, vector<int> &p, int fee, int n, vector<vector<int>> &dp){
-        if(idx>=n) return 0;
-
-        if(dp[idx][buy] != -1) return dp[idx][buy];
-        if(buy){
-            return dp[idx][buy] = max(-p[idx] + cnt(idx+1, 0, p, fee, n, dp),cnt(idx+1, 1, p, fee, n, dp));
-        }
-        else return dp[idx][buy] = max(p[idx] -fee + cnt(idx+1, 1, p, fee, n, dp),cnt(idx+1, 0, p, fee, n, dp));
-    }
-
     int maxProfit(vector<int>& p, int fee) {
         int n = p.size();
 
-        vector<vector<int>> dp(n, vector<int> (2, -1));
+        int aheadNotBuy, aheadBuy, curBuy, curNotBuy;
 
-        return cnt(0, 1, p, fee, n, dp);
+        aheadNotBuy = aheadBuy = 0;
+
+        for(int i = n-1; i>=0; i--){
+            curNotBuy = max(p[i] + aheadBuy, aheadNotBuy);
+            curBuy = max(-p[i] - fee + aheadNotBuy, aheadBuy);
+
+            aheadBuy = curBuy;
+            aheadNotBuy = curNotBuy;
+        }
+        return aheadBuy;
     }
 };
