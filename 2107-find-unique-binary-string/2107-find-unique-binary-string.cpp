306013ab5a0da6316e952a52b194b9552a267bf5
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-21 02:23:42 +0530

    Time: 0 ms (100%), Space: 13.8 MB (24.17%) - LeetHub

--- 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
+++ 2107-find-unique-binary-string/2107-find-unique-binary-string.cpp
@@ -5,13 +5,16 @@ public:
 
     string rec(string cur){
         if(cur.size() == n){
-            if(st.find(cur) == st.end()){
-                return cur;
+                if(st.find(cur) == st.end()){
+                    return cur;
             }
+
             return "";
+
         }
 
         string addZero = rec(cur + "0");
+
         if(addZero.size() > 0) {
             return addZero;
         }
@@ -22,7 +25,6 @@ public:
     string findDifferentBinaryString(vector<string>& nums) {
         n = nums.size();
         st.clear();
-
         for(string s : nums){
             st.insert(s);
         }
