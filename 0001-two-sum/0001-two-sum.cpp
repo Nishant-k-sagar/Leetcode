bc291aafaa95ba15ebbc9478764f50a9300dcb30
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-23 17:47:25 +0530

    Time: 3 ms (73.94%), Space: 14.6 MB (55.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,17 +3,31 @@ public:
     vector<int> twoSum(vector<int>& nums, int target) {
         int n = nums.size();
 
-        map<int, int> mp;
+        vector<int> orig = nums;
+        sort(nums.begin(), nums.end());
 
-        for(int i=0; i<n; i++){
-            int num = nums[i];
-            int Needed = target - num;
-
-            if(mp.find(Needed) != mp.end()){
-                return {mp[Needed], i};
+        int j = n-1;
+        int i = 0;
+        int el1 = INT_MIN;
+        int el2 = INT_MIN;
+        while(i<j){
+            int sum = nums[i] + nums[j];
+            if(sum == target){
+                el1 = nums[i];
+                el2 = nums[j];
+                break;
             }
-            mp[num] = i;
+            else if(sum>target){
+                j--;
+            }
+            else i++;
         }
-        return {};
+        int idx1 = -1;
+        int idx2 = -1;
+        for(int i = 0; i<n; i++){
+            if(el1 == orig[i] && idx1 == -1)idx1 = i;
+            else if(el2 == orig[i] && idx2 == -1)idx2 = i;
+        }
+        return {idx1, idx2};
     }
 };
