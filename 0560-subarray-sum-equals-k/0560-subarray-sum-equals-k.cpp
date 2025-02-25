26f638c85375f8c3d9721e992e8b93f486ff8b6b
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-02-25 14:17:49 +0530

    Time: 91 ms (28.15%), Space: 54 MB (5.44%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -4,13 +4,13 @@ public:
         int cnt = 0;
         int sum = 0;
         int n = nums.size();
-        map<int, int> mp;
+        map <int, int> mp;
         mp[0] = 1;
         for(int i=0;  i<n; i++){
             sum+=nums[i];
-            if(mp.find(sum-k)!=mp.end()){
+            // if(mp.find(sum-k)!=mp.end()){
                 cnt+=mp[sum-k];
-            }
+            // }
             if (mp.find(sum)!=mp.end()) {
                 mp[sum] = mp[sum]+1;
             } 
