1f06148835d9784f31415306605382e9dc012354
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-04-16 16:36:39 +0530

    Time: 1 ms (43.92%), Space: 13.4 MB (65.33%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cpp
+++ 0137-single-number-ii/0137-single-number-ii.cpp
@@ -4,12 +4,16 @@ public:
         int n = nums.size();
 
         sort(nums.begin(), nums.end());
-        
-        for(int i=0; i<n; i+=3){
-            if(i == n-1 || nums[i]!=nums[i+1]){
+
+        if(n == 1)return nums[0];
+
+        if(nums[0]!=nums[1])return nums[0];
+
+        for(int i=1; i<n-1; i++){
+            if(nums[i]!=nums[i+1] && nums[i] != nums[i-1]){
                 return nums[i];
             }
         }
-        return 0;
+        return nums[n-1];
     }
 };
