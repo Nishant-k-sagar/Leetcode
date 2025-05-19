e3a588960705ce7504812db33258b98694e0e06c
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-05-19 16:53:07 +0530

    Time: 0 ms (100%), Space: 14.6 MB (49.45%) - LeetHub

--- 0130-surrounded-regions/0130-surrounded-regions.cpp
+++ 0130-surrounded-regions/0130-surrounded-regions.cpp
@@ -1,57 +1,46 @@
 class Solution {
-    private:
-    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &mat, int delRow[], int delCol[]){
-        vis[row][col] = 1;
-        int n = mat.size();
-        int m = mat[0].size();
+private:
+    void dfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>> &mat, int delRow[], int delCol[]) {
+        vis[row][col] = 1; // Mark visited
         
-        for(int i=0; i<4; i++){
-            int nrow = row + delRow[i];
-            int ncol = col + delCol[i];
+        int n = mat.size(), m = mat[0].size();
+        
+        // Explore up, down, left, right
+        for (int i = 0; i < 4; i++) {
+            int nrow = row + delRow[i], ncol = col + delCol[i];
             
-            if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && !vis[nrow][ncol] && mat[nrow][ncol] == 'O'){
-                dfs(nrow, ncol, vis, mat, delRow, delCol);
+            // voundary conditions and other checks
+            if (nrow < n && nrow >= 0 && ncol < m && ncol >= 0 && !vis[nrow][ncol] && mat[nrow][ncol] == 'O') {
+                dfs(nrow, ncol, vis, mat, delRow, delCol); // dfs
             }
         }
     }
 
 public:
     void solve(vector<vector<char>>& mat) {
-        int n = mat.size();
-        int m = mat[0].size();
+        int n = mat.size(), m = mat[0].size();
         
-        int delRow[] = {0, -1, 0, 1};
-        int delCol[] = {1, 0, -1, 0};
+        int delRow[] = {0, -1, 0, 1}, delCol[] = {1, 0, -1, 0};
         
-        vector<vector<int>> vis(n, vector<int> (m, 0));
+        vector<vector<int>> vis(n, vector<int>(m, 0));
         
-        for(int j = 0; j<m; j++){
-            if(!vis[0][j] && mat[0][j] == 'O'){
-                dfs(0, j, vis, mat, delRow, delCol);
-            }
-            
-            if(!vis[n-1][j] && mat[n-1][j] == 'O'){
-                dfs(n-1, j, vis, mat, delRow, delCol);
-            }
+        // for first and last rows
+        for (int j = 0; j < m; j++) {
+            if (!vis[0][j] && mat[0][j] == 'O') dfs(0, j, vis, mat, delRow, delCol);
+            if (!vis[n-1][j] && mat[n-1][j] == 'O') dfs(n-1, j, vis, mat, delRow, delCol);
         }
         
-        for(int i = 0; i<n; i++){
-            if(!vis[i][0] && mat[i][0] == 'O'){
-                dfs(i, 0, vis, mat, delRow, delCol);
-            }
-            
-            if(!vis[i][m-1] && mat[i][m-1] == 'O'){
-                dfs(i, m-1, vis, mat, delRow, delCol);
-            }
+        //for first and last cols
+        for (int i = 0; i < n; i++) {
+            if (!vis[i][0] && mat[i][0] == 'O') dfs(i, 0, vis, mat, delRow, delCol);
+            if (!vis[i][m-1] && mat[i][m-1] == 'O') dfs(i, m-1, vis, mat, delRow, delCol);
         }
         
-        for(int i = 0; i < n; i++){
-            for(int j = 0; j < m; j++){
-                if(!vis[i][j] && mat[i][j] == 'O')
-                    mat[i][j] = 'X';
+        // Replace unvisited 'O's with 'X's
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!vis[i][j] && mat[i][j] == 'O') mat[i][j] = 'X';
             }
         }
-        
-        // return mat;
     }
 };
