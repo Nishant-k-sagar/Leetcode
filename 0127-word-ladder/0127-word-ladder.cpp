91dfc031abb10ee7c478006fa2c42f234d94f7e0
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-05-20 12:37:50 +0530

    Time: 48 ms (88.62%), Space: 21.1 MB (63.6%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -3,30 +3,30 @@ public:
     int ladderLength(string startWord, string targetWord, vector<string>& wordList) {
         queue<pair<string, int>> q;
         q.push({startWord, 1});
-        
+
         unordered_set<string> st(wordList.begin(), wordList.end());
         st.erase(startWord);
-        
-        while(!q.empty()){
+
+        while (!q.empty()) {
             string word = q.front().first;
             int steps = q.front().second;
             q.pop();
+
+            if (word == targetWord) return steps; // Found the target word
             
-            if(word == targetWord)return steps;
-            
-            for(int i=0; i<word.size(); i++){
-                char original = word[i]; //restores the i-th letter in word after it gets changed
-                for(char ch = 'a'; ch <= 'z'; ch++){
+            for (int i = 0; i < word.size(); i++) {
+                char original = word[i]; // Restores the i-th letter in word after it gets changed
+                for (char ch = 'a'; ch <= 'z'; ch++) {
                     word[i] = ch;
-                    
-                    if(st.find(word) != st.end()){
-                        st.erase(word);
+
+                    if (st.find(word) != st.end()) { // Valid transformation
+                        st.erase(word); // Remove word to avoid revisiting
                         q.push({word, steps + 1});
                     }
                 }
-                word[i] = original;
+                word[i] = original; // Restore original character
             }
         }
-        return 0;
+        return 0; // not found
     }
 };
