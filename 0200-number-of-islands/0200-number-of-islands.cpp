d17860860f26acc4269be869928ee78c9e261fea
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-05-21 16:06:35 +0530

    Time: 33 ms (25.49%), Space: 24.5 MB (17.87%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,51 +1,49 @@
-namespace std {
-template <> struct hash<pair<int, int>> {
-    size_t operator()(const pair<int, int>& p) const {
-        return hash<int>()(p.first) ^ (hash<int>()(p.second) << 1);
-    }
-};
-}
-
 class Solution {
 private:
-    void bfs(vector<vector<char>>& grid, int i, int j,
-             unordered_set<pair<int, int>>& vis, vector<pair<int, int>>& dirs,
-             int r, int c) {
+    void bfs(int row, int col, vector<vector<int>> &vis, vector<vector<char>>& grid){
+        vis[row][col] = 1;
         queue<pair<int, int>> q;
-        vis.insert({i, j});
-        q.push({i, j});
 
-        while (!q.empty()) {
-            auto [row, col] = q.front();
+        q.push({row, col});
+
+        int n = grid.size();
+        int m = grid[0].size();
+
+        while(!q.empty()){
+            int row = q.front().first;
+            int col = q.front().second;
             q.pop();
 
-            for (auto [dr, dc] : dirs) {
-                int nr = row + dr;
-                int nc = col + dc;
+            int delrow[] = {0, -1, 0, 1};
+            int delcol[] = {-1, 0, 1, 0};
+            
+            for(int i=0; i<4; i++){
+                    int nrow = row + delrow[i];
+                    int ncol = col + delcol[i];
 
-                if (nr >= 0 && nr < r && nc >= 0 && nc < c &&
-                    grid[nr][nc] == '1' && vis.find({nr, nc}) == vis.end()) {
-                    q.push({nr, nc});
-                    vis.insert({nr, nc});
+                    if(nrow<n && nrow>=0 && ncol<m && ncol>=0 && vis[nrow][ncol] == 0 && grid[nrow][ncol] == '1'){
+                        vis[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                    }
                 }
-            }
         }
+
     }
 
 public:
     int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+
         int cnt = 0;
-        int r = grid.size();
-        int c = grid[0].size();
 
-        unordered_set<pair<int, int>> vis;
-        vector<pair<int, int>> dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
-                if (grid[i][j] == '1' && vis.find({i, j}) == vis.end()) {
+        for(int i = 0; i<n; i++){
+            for(int j = 0; j<m; j++){
+                if(vis[i][j] == 0 && grid[i][j] == '1'){
                     cnt++;
-                    bfs(grid, i, j, vis, dirs, r, c);
+                    bfs(i, j, vis, grid);
                 }
             }
         }
