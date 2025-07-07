f042f2b2833ae01888f7c07b1153ae5f826e4bb8
Nishant-k-sagar <147799872+Nishant-k-sagar@users.noreply.github.com>
2025-07-07 17:56:49 +0530

    Time: 11 ms (74.62%), Space: 29.1 MB (65.58%) - LeetHub

--- 0475-heaters/0475-heaters.cpp
+++ 0475-heaters/0475-heaters.cpp
@@ -3,25 +3,52 @@ public:
     int findRadius(vector<int>& houses, vector<int>& heaters) {
         sort(houses.begin(), houses.end());
         sort(heaters.begin(), heaters.end());
-        int r = 0;
+        int rad = 0;
 
         for(int h : houses){
-            auto it = lower_bound(heaters.begin(), heaters.end(), h);
+            // auto it = lower_bound(heaters.begin(), heaters.end(), h);
 
-            int dist1 = INT_MAX;
+            // int dist1 = INT_MAX;
 
-            if(it != heaters.end()){
-                dist1 = abs(*it - h);
+            // if(it != heaters.end()){
+            //     dist1 = abs(*it - h);
+            // }
+
+            // int dist2 = INT_MAX;
+            // if(it != heaters.begin()){
+            //     dist2 = abs(*(it - 1) - h);
+            // }
+
+            // int mini = min(dist1, dist2);
+            // r = max(r, mini);
+
+            int l = 0;
+            int r = heaters.size()-1;
+            int mini = INT_MAX;
+
+            while(l <= r){
+                int mid = l + (r - l)/2;
+
+                if(heaters[mid] == h){
+                    mini = 0;
+                    break;
+                }
+                else if(heaters[mid] < h){
+                    l = mid + 1;
+                }
+                else{
+                    r = mid - 1;
+                }
             }
 
-            int dist2 = INT_MAX;
-            if(it != heaters.begin()){
-                dist2 = abs(*(it - 1) - h);
+            if(l < heaters.size()){
+                mini = min(mini, abs(heaters[l] - h));
             }
-
-            int mini = min(dist1, dist2);
-            r = max(r, mini);
+            if(r >= 0){
+                mini = min(mini, abs(heaters[r] - h));
+            }
+            rad = max(rad, mini);
         }
-        return r;
+        return rad;
     }
 };
